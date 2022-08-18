2ddd7b19e198a8e3d7d7674c50191deac742b7a4
SergejTkachenko <tks.emailbox@gmail.com>
2022-08-18 14:12:34 +0300

    Initial commit

---
+++ src/main/java/com/tkc/spring/springboot/spring_data_jpa/service/EmployeeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.tkc.spring.springboot.spring_data_jpa.service;
+
+import com.tkc.spring.springboot.spring_data_jpa.dao.EmployeeRepository;
+import com.tkc.spring.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+
+        Employee employee = null;
+
+        Optional<Employee> optional = employeeRepository.findById(id);
+        if (optional.isPresent()) {
+            employee = optional.get();
+        }
+
+        return employee;
+    }
+    @Override
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Employee> findAllByName(String name) {
+        List<Employee> employees = employeeRepository.findAllByName(name);
+        return employees;
+    }
+}
